a5511e80b113e0edced8ad1bb4c997e281421837
LG <34185919+projektorius96@users.noreply.github.com>
2025-03-20 01:02:11 +0200

    follow-up: some getter|setter pairs added

--- implementation/index.js
+++ implementation/index.js
@@ -50,61 +50,64 @@ export const transformSVG = ({HTMLCanvas, XMLSVG, parent}) =>{
             scalingFactor = 2
             ;
 
-        Array.from(svgElement.children).on((shape, i) => {
+        Array.from(svgElement.children).on((shape) => {
 
-            switch (shape.tagName) {
-                case 'rect':
-                    void function(){
-                        
-                        if ( registerSettersFor(shape) ) {
-                            shape.setTranslate({x: Number(stage.grid.SVG.X_IN_MIDDLE), y: Number(stage.grid.SVG.Y_IN_MIDDLE)});
-                            shape.setArea({width: stage.grid.GRIDCELL_DIM * scalingFactor, height: stage.grid.GRIDCELL_DIM * scalingFactor});
-                        }                        
-                        
-                        /**
-                         * 
-                         * > **NOTE**: In order to avoid string coercion whilst accessing values via `.attributes` property, make sure you're setting values on the right hand side of expression using explicit `Number()` contructor !
-                         */
-                        let NOTE;
-                        XMLSVG.Helpers.findBy(`${shape.tagName}-2`).attributes.x.value 
-                            = Number( XMLSVG.Helpers.findBy(`${shape.tagName}-1`).attributes.x.value ) + Number( XMLSVG.Helpers.findBy(`${shape.tagName}-1`).attributes.width.value );
-                        XMLSVG.Helpers.findBy(`${shape.tagName}-2`).attributes.y.value 
-                            = Number( XMLSVG.Helpers.findBy(`${shape.tagName}-1`).attributes.y.value ) + Number( XMLSVG.Helpers.findBy(`${shape.tagName}-1`).attributes.height.value );
+            if ( registerSettersFor( shape ) ) {
 
-                    }();
-                break;
-                case 'circle':
-                    void function(){
-
-                        shape.attributes.cx.value = Number(stage.grid.SVG.X_IN_MIDDLE)
-                        shape.attributes.cy.value = Number(stage.grid.SVG.Y_IN_MIDDLE);
-                        shape.attributes.r.value = Number(stage.grid.GRIDCELL_DIM) * scalingFactor / 8
-
-                    }();
-                break;
-                case 'path':
-                    void function () {
-
-                        shape.style.strokeWidth = 1 / stage.grid.GRIDCELL_DIM;
-                        
-                        const currentMatrix = (cursorFeedback) => {
-
-                            const matrix = new DOMMatrix(
-                                HTMLCanvas.Helpers.Trigonometry.setTransform(-45, (cursorFeedback?.x || Number(stage.grid.SVG.X_IN_MIDDLE)), (cursorFeedback?.y || Number(stage.grid.SVG.Y_IN_MIDDLE)))
-                            ); matrix.scaleSelf(stage.grid.GRIDCELL_DIM * ( 1 / Math.cos( Math.PI/4 ) ) , stage.grid.GRIDCELL_DIM * ( 1 / Math.cos( Math.PI/4 ) ));
+                switch (shape.tagName) {
+                    case 'rect':
+                        void function(){
+                                
+                                shape.setTranslate({x: Number(stage.grid.SVG.X_IN_MIDDLE), y: Number(stage.grid.SVG.Y_IN_MIDDLE)});
+                                shape.setArea({width: stage.grid.GRIDCELL_DIM * scalingFactor, height: stage.grid.GRIDCELL_DIM * scalingFactor});
+    
+                                if ( shape.id === `rect-2` ) {
+                                        
+                                    shape.setTranslate({
+                                        x: Number( XMLSVG.Helpers.findBy(`${shape.tagName}-1`).getTranslate().x ) + Number( XMLSVG.Helpers.findBy(`${shape.tagName}-1`).getArea().width ), 
+                                        y: Number( XMLSVG.Helpers.findBy(`${shape.tagName}-1`).getTranslate().y ) + Number( XMLSVG.Helpers.findBy(`${shape.tagName}-1`).getArea().height )
+                                    })
+    
+                                }
+    
+                        }();
+                    break;
+                    case 'circle':
+                        void function(){
+    
+                            shape.setCircle({
+                                cx: Number(stage.grid.SVG.X_IN_MIDDLE),
+                                cy: Number(stage.grid.SVG.Y_IN_MIDDLE),
+                                r: Number(stage.grid.GRIDCELL_DIM) * (scalingFactor / 8)
+                            })
+    
+                        }();
+                    break;
+                    case 'path':
+                        void function () {
+    
+                            shape.style.strokeWidth = 1 / stage.grid.GRIDCELL_DIM;
                             
-                            return matrix;
-
-                        }
-
-                        shape.setAttribute("transform", currentMatrix().toString());
-
-                        shape.addEventListener('click', (e) => {
-                            XMLSVG.enableDraggingFor(e.currentTarget, currentMatrix.bind(null))
-                        });
-                        
-                    }();
-                break;
+                            const currentMatrix = (cursorFeedback) => {
+    
+                                const matrix = new DOMMatrix(
+                                    HTMLCanvas.Helpers.Trigonometry.setTransform(-45, (cursorFeedback?.x || Number(stage.grid.SVG.X_IN_MIDDLE)), (cursorFeedback?.y || Number(stage.grid.SVG.Y_IN_MIDDLE)))
+                                ); matrix.scaleSelf(stage.grid.GRIDCELL_DIM * ( 1 / Math.cos( Math.PI/4 ) ) , stage.grid.GRIDCELL_DIM * ( 1 / Math.cos( Math.PI/4 ) ));
+                                
+                                return matrix;
+    
+                            }
+    
+                            shape.setAttribute("transform", currentMatrix().toString());
+    
+                            shape.addEventListener('click', (e) => {
+                                XMLSVG.enableDraggingFor(e.currentTarget, currentMatrix.bind(null))
+                            });
+                            
+                        }();
+                    break;
+                }
+            
             }
 
             shape.addEventListener('click', (e)=>{
@@ -122,14 +125,45 @@ function registerSettersFor(svgShape){
     switch (svgShape.tagName) {
         case 'rect':
             Object.assign(svgShape, {
+                getTranslate(){
+                    return ({
+                        x: svgShape.attributes.x.value,
+                        y: svgShape.attributes.y.value
+                    })
+                }
+                ,
                 setTranslate({x=0, y=0}){
                     svgShape.attributes.x.value = x;
                     svgShape.attributes.y.value = y;
                 }
                 ,
+                getArea(){
+                    return ({
+                        width: svgShape.attributes.width.value  ,
+                        height: svgShape.attributes.height.value,
+                    })
+                }
+                ,
                 setArea({width=1, height=1}){
                     svgShape.attributes.width.value = width;
                     svgShape.attributes.height.value = height;
+                }
+            })
+        break;
+        case 'circle':
+            Object.assign(svgShape, {
+                getCircle(){
+                    return ({
+                        cx: svgShape.attributes.cx.value, 
+                        cy: svgShape.attributes.cy.value, 
+                        r: svgShape.attributes.r.value
+                    })
+                }
+                ,
+                setCircle({cx=0, cy=0, r=1}){
+                    svgShape.attributes.cx.value = cx
+                    svgShape.attributes.cy.value = cy;
+                    svgShape.attributes.r.value = r;
                 }
             })
         break;
