19ca8df04db4a549b0a1e7d0467db06cbc1c68d4
LG <34185919+projektorius96@users.noreply.github.com>
2025-04-05 22:07:38 +0300

    element was missing '.name' accessor in 'waveConfig': this happended due to my own fault whilst revising the code

--- src/views/xmlsvg/DOMutils.js
+++ src/views/xmlsvg/DOMutils.js
@@ -1,2 +1,3 @@
 EventTarget.prototype.on = EventTarget.prototype.addEventListener;
 EventTarget.prototype.rm = EventTarget.prototype.removeEventListener;
+EventTarget.prototype.dispatch = EventTarget.prototype.dispatchEvent;
